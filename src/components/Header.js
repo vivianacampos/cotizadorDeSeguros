30b0d51aab72b51837e882f652bee6497181bf74
vivianacampos <vivianacampos@ugm.cl>
2021-02-16 21:58:21 -0300

    inicio proyecto

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = ({titulo}) => {
+    return (  
+        <header>
+            <h1>{titulo}</h1>
+        </header>
+
+    );
+}
+
+export default Header;
